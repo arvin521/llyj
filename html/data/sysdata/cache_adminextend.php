2543bad7674866f7f6dedf70453216f23cdb7d6a
wenjie.wang <wenjie.wang@aispeech.com>
2017-11-08 09:41:02 +0800

    rm all

--- html/data/sysdata/cache_adminextend.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 5570324a52b71a69c6f012eb40e84564
-
-$adminextend = array (
-  0 => 'cloud.php',
-);
-?>
--- html/data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: b53072f698f7f07ba9638e39e80168df
-
-$pluginsetting = array (
-);
-?>
